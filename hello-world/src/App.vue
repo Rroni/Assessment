80a5d41ecaf793e3583ea420e24ffdd33ee4f5e3
Rroni <rroni.hoxha@gmail.com>
2024-04-19 11:25:49 +0200

    Remove HelloWorld, create GameTable component

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GamesTable />
   </div>
 </template>
-
+ß
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import GamesTable from "@/components/GamesTable.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GamesTable
   }
 }
 </script>
